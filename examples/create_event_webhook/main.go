f42231fa59e03abe5653dbb355ef474fa4251b2c
Kenzo Tanaka <kenzo.tanaka0107@gmail.com>
2024-03-17 23:48:59 +0900

    Support Event Webhook API
    - `GET /v3/user/webhooks/event/settings/{id}`: [Get an Event Webhook](https://docs.sendgrid.com/api-reference/webhooks/get-an-event-webhook)
    - `GET /v3/user/webhooks/event/settings/all`: [Get All Event Webhooks](https://docs.sendgrid.com/api-reference/webhooks/get-all-event-webhooks)
    - `POST /v3/user/webhooks/event/settings`: [Create an Event Webhook](https://docs.sendgrid.com/api-reference/webhooks/create-an-event-webhook)
    - `PATCH /v3/user/webhooks/event/settings/{id}`: [Update an Event Webhook](https://docs.sendgrid.com/api-reference/webhooks/update-an-event-webhook)
    - `DELETE /v3/user/webhooks/event/settings/{id}`: [Delete an Event Webhook](https://docs.sendgrid.com/api-reference/webhooks/delete-an-event-webhook)

---
+++ examples/create_event_webhook/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/kenzo0107/sendgrid"
+)
+
+func main() {
+	if err := handler(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler() error {
+	apiKey := os.Getenv("SENDGRID_API_KEY")
+
+	c := sendgrid.New(apiKey, sendgrid.OptionDebug(true))
+	r, err := c.CreateEventWebhook(context.TODO(), &sendgrid.InputCreateEventWebhook{
+		URL: "https://example.com",
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("%#v\n", r)
+
+	return nil
+}
